3ed33cd8b2c6595ce8dfa617abbfea9cd6f5501f
abdol <abdol@REZA>
2024-03-30 14:01:53 +0330

    Add project files.

---
+++ Model/Bug.cs
@@ -0,0 +1,9 @@
+﻿namespace firstCRUD.Model;
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string Name  { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+
+    }
